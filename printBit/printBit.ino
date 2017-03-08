3b063cad4ff82213122339e3c37eaa6e911c7dce
Kristian <din_dr_elvis@live.se>
2017-03-08 13:45:37 +0100

    This is a game where hit the led by pressing a button before it goes out of bounds

--- printBit/printBit.ino
+++ printBit/printBit.ino
@@ -1,32 +1,86 @@
+const int timer = 500; //
+int gameOverTimer;//
 int pins[3];
+//Set game variables
 int counter = 0;
 boolean countUp = true;
+boolean gameOver = false;
+
+#define onOffPin 7
+#define buttonOne 2
+#define buttonTwo 3
+
+unsigned long buttonOneClicked = 0;
+unsigned long buttonTwoClicked = 0;
 
 
 void setup() {
+  pinMode(buttonOne, INPUT_PULLUP);
+  pinMode(buttonTwo, INPUT_PULLUP);
+  pinMode(onOffPin, OUTPUT);
+  digitalWrite(onOffPin, HIGH);
+  
   for (int i = 0; i < 3; i++) {
     //Assign number of pins
     pins[i] = i + 4;
     // initialize the LED pins as an outputs:
     pinMode(pins[i], OUTPUT);
   }
+  attachInterrupt(digitalPinToInterrupt(buttonOne), hitOne, FALLING);
+  attachInterrupt(digitalPinToInterrupt(buttonTwo), hitTwo, FALLING);
   Serial.begin(9600);
 }
 
 void loop() {
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(pins[i], bitRead(counter, i));
-    //Serial.println(bitRead(counter, i));
+  writeCounter();
+  
+  if (counter < 0 || counter > 7 && !gameOver) {//If the counter is outside of screen and game is not already set to gameovermode then set to gameovermode
+    setGameOver();
   }
+  Serial.println(counter);
+  delay(1000); //wait 1s
   if (countUp){
     counter++;
   }
   else {
     counter--;
   }
-  if (counter == 0 || counter == 7){
+}
+void hitOne() {
+  if (counter == 0 && !countUp && buttonOneClicked - millis() > timer) {
+    buttonOneClicked = millis();
     countUp = !countUp;
   }
-  Serial.println(counter);
-  delay(1000); //wait 1s
+  resetGame();
 }
+
+void hitTwo() {
+  if (counter == 7 && countUp && buttonTwoClicked - millis() > timer) {
+    buttonTwoClicked = millis();
+    countUp = !countUp;
+  }
+  resetGame();
+}
+void resetGame() {
+  if (gameOverTimer - millis() > timer && gameOver) {//Reset game variables
+    counter = 0;
+    gameOver = false;
+    countUp = true;
+    writeCounter();
+    digitalWrite(onOffPin, HIGH);
+  }
+}
+
+void setGameOver() {
+  gameOver = true;
+  gameOverTimer = millis();
+  digitalWrite(onOffPin, LOW);
+}
+
+void writeCounter() {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(pins[i], bitRead(counter, i));
+    //Serial.println(bitRead(counter, i));
+  }
+}
+
