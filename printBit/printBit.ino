3c212b4fa6822936ad887bb7e39249a5bdefecec
Kristian <din_dr_elvis@live.se>
2017-04-26 14:04:57 +0200

    Added comments

--- printBit/printBit.ino
+++ printBit/printBit.ino
@@ -1,41 +1,40 @@
 const int timer = 300; //The minimum time beetween every press
 unsigned long gameOverTimer;//Save when last game over event occured
 
-#define latchPin 4
-#define clockPin 5
-#define dataPin 6
+#define latchPin 4 //This pin is used to lock leds while shifing out bits to the register
+#define clockPin 5 //This pin decides the shiftregisters data clock frequency
+#define dataPin 6 //The pin to shift out data to
 
-//Force shift to next shift register
-//#define latchPin1 7 
-//#define clockPin1 8
-//#define dataPin 9
+//Force shift to next shift register by shifting out 16bits
+
 
 //Set game variables
-int counter = 0;
-boolean countUp = true;
-boolean gameOver = false;
-boolean scorePhase = false;
+int counter = 0; //Where is the puck? Saved in this variable
+boolean countUp = true;//What direction is the puck moving? Saved in this variable
+boolean gameOver = false; //Keeps track if the game is playing or not. If gameOver is false then continue playing
+boolean scorePhase = false; //If true show score
 int scorePlayerOne = 0;
 int scorePlayerTwo = 0;
-int ledSpeed = 500;
+int ledSpeed = 500; //Puck speed
 
-#define buttonOne 2
-#define buttonTwo 3
+#define buttonOne 2 //Player one button
+#define buttonTwo 3 //Player two button
 
-unsigned long buttonOneClicked = 0;
-unsigned long buttonTwoClicked = 0;
+unsigned long buttonOneClicked = 0;//Saves when was the last confirmed press from player one
+unsigned long buttonTwoClicked = 0;//Saves when was the last confirmed press from player two
 
 
 void setup() {
-  pinMode(buttonOne, INPUT_PULLUP);
+  pinMode(buttonOne, INPUT_PULLUP);//Setup buttons with pullup
   pinMode(buttonTwo, INPUT_PULLUP);
-  pinMode(latchPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);//Setup pins for the shift register
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-  
-  attachInterrupt(digitalPinToInterrupt(buttonOne), hitOne, FALLING);
-  attachInterrupt(digitalPinToInterrupt(buttonTwo), hitTwo, FALLING);
-  Serial.begin(9600);
+
+  //Interupts can even interrupt during the command delay()
+  attachInterrupt(digitalPinToInterrupt(buttonOne), hitOne, FALLING);//Listen for Falling (High then Low) on playerOne pin. Then run the hitOne function
+  attachInterrupt(digitalPinToInterrupt(buttonTwo), hitTwo, FALLING);//Listen for Falling (High then Low) on playerTwo pin. Then run the hitTwo function
+  Serial.begin(9600);//Start serial communication at 9600bit/s. Used for debugging
 }
 
 void loop() {
@@ -43,79 +42,80 @@ void loop() {
   if (counter < 0 || counter > 15 && !gameOver) {//If the counter is outside of screen and game is not already set to gameovermode then set to gameovermode
     setGameOver();
   }
-  Serial.println(counter);
+  Serial.println(counter);//Write where the puck is
   
-  if (!gameOver) {//If not game over continue the game
-    writeToRegister(prepareForRegister(counter));
-    delay(ledSpeed); //wait
+  if (!gameOver) {//If not game over continue the game and move the puck each second
+    writeToRegister(prepareForRegister(counter)); //Use the counter to decide which bit should be one. Then write the 2 bytes to the register 
+    delay(ledSpeed); //wait. countUp can be changed during this time
     if (countUp){
-      counter++;
+      counter++; //Move puck up
     }
     else {
-      counter--;
+      counter--; //Move puck down
     }
   }
-  if (gameOver && !scorePhase) {
-    if (counter == 15) writeToRegister(prepareScoreForRegister(0, 8));
+  if (gameOver && !scorePhase) {//Blink five times then go to score mode
+    if (counter == 15) writeToRegister(prepareScoreForRegister(0, 8)); //Turn on all 8 leds on the losing field
     if (counter == 0) writeToRegister(prepareScoreForRegister(8, 0));
     delay(100); //wait
-    writeToRegister(0);
+    writeToRegister(0);//turn all leds off
     delay(100); //wait
-    if (millis() - gameOverTimer > 1000) scorePhase = true;
+    if (millis() - gameOverTimer > 1000) scorePhase = true; //Show the score after one second
   }
-  if (gameOver && scorePhase) {
-    writeToRegister(prepareScoreForRegister(scorePlayerOne, scorePlayerTwo));
+  if (gameOver && scorePhase) {//Score mode.Displays the current score
+    writeToRegister(prepareScoreForRegister(scorePlayerOne, scorePlayerTwo)); //Convert the score to a binary number where the score is represented by the amount of leds in High state and then shift out the data
   }
 }
 void hitOne() {
-  if (counter == 0 && !countUp && millis() - buttonOneClicked> timer && !gameOver) {
-    buttonOneClicked = millis();
-    countUp = !countUp;
-    ledSpeed *= 0.8;
+  if (counter == 0 && !countUp && millis() - buttonOneClicked> timer && !gameOver) {//Checks if puck is whithin reach also checks if player is allowed to hit the puck right now
+    countUp = !countUp;//Change direction of the puck
+    ledSpeed *= 0.8;//Increase puck speed by decreasing the time it waits between each puck movement
   }
-  resetGame();
+  buttonOneClicked = millis();//Save when the press occurred
+  resetGame();//If allowed reset the game to continue playing. Score is still saved
 }
 
 void hitTwo() {
-  if (counter == 15 && countUp && millis() - buttonTwoClicked > timer && !gameOver) {
-    buttonTwoClicked = millis();
-    countUp = !countUp;
-    ledSpeed *= 0.8;
+  if (counter == 15 && countUp && millis() - buttonTwoClicked > timer && !gameOver) {//Checks if puck is whithin reach also checks if player is allowed to hit the puck right now
+    countUp = !countUp;//Change direction of the puck
+    ledSpeed *= 0.8;//Increase puck speed by decreasing the time it waits between each puck movement
   }
-  resetGame();
+  buttonTwoClicked = millis();//Save when the press occurred
+  resetGame();//If allowed reset the game to continue playing. Score is still saved
 }
 void resetGame() {
-  if (millis() - gameOverTimer > 2000 && gameOver && scorePhase) {//Reset game variables
-    if (counter >= 15){
+  if (millis() - gameOverTimer > 2000 && gameOver && scorePhase) {//if allowed reset all game variables except the score
+    if (counter >= 15){//Decide who serves. Player one
       counter = 0;
       countUp = true;
     }
-    else if (counter <= 0){
+    else if (counter <= 0){//Decide who serves. Player two
       counter = 15;
       countUp = false;
     }
-    scorePhase = false;
-    gameOver = false;
-    writeToRegister(prepareForRegister(counter));
+    ledSpeed = 500;//Reset ledSpeed
+    scorePhase = false;//Do not show score while the game is on
+    gameOver = false;//Continue the game
+    writeToRegister(prepareForRegister(counter));//Shift out puck position now for instant response
   }
 }
 
 void setGameOver() {
-  ledSpeed = 500;
-  if (counter > 15){
-    scorePlayerOne ++;
-    counter = 15; 
+  //ledSpeed = 500; This is now done in the reset function
+  if (counter > 15){//Check who scored
+    scorePlayerOne ++;//Add a point to player one
+    counter = 15; //Keep track off who scored
   }
-  if (counter < 0){
-    scorePlayerTwo ++;
-    counter = 0; 
+  if (counter < 0){//Check who scored
+    scorePlayerTwo ++;//Add a point to player Two
+    counter = 0; //Keep track off who scored
   }
-  gameOver = true;
-  gameOverTimer = millis();
+  gameOver = true; //Stop playing
+  gameOverTimer = millis();//Saves when goal occurred
   tone(11, 50, 1000); //Play a tone on pin 11 for 1s
 }
 
-unsigned int prepareForRegister(int pos){
+unsigned int prepareForRegister(int pos){//Returns a variable that has a 1 in the position mentioned in pos.
    unsigned int raised = power(2,pos);
    //Serial.println(pos);
    //Serial.println(raised,2);
@@ -123,26 +123,26 @@ unsigned int prepareForRegister(int pos){
 }
 
 unsigned int prepareScoreForRegister(int score1, int score2){//Put both scores in the same variable
-   unsigned int raised = power(2 , score2) - 1;
-   if (raised > 0) while(raised < 32768) {
+   unsigned int raised = power(2 , score2) - 1; //This will make the amount of ones equal to the score
+   if (raised > 0) while(raised < 32768) {//move the score until it has reached the 16th bit. This is only possible if more than zero was written to the variable.
       raised = raised << 1;
    }
-   raised += power(2 , score1) - 1;
-   Serial.println(raised,2);
+   raised += power(2 , score1) - 1; // add the score from player one
+   Serial.println(raised,2); // Print the binary number in binary to serial
    return raised;
 }
 
-unsigned int power(int base, int exponent){
-  if (exponent == 0) {
+unsigned int power(int base, int exponent){//Used as the pow() function except this one only works with integers and also returns a int.
+  if (exponent == 0) {//define position zero in this recursiv function
     base = 1;
   }
-  else {
+  else {//define the recursiv expression
     base = base * power(base, exponent - 1);
   }
   return base;
 }
 
-void writeToRegister(unsigned int number) {
+void writeToRegister(unsigned int number) {//Shifts out the last two bytes to the shift registers
   
     digitalWrite(latchPin, LOW);   //Lock the outputs on the register
     shiftOut(dataPin, clockPin, MSBFIRST, number >> 8); //Write 
